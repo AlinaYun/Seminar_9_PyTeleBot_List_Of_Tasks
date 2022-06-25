61debdf938c8c7199c82b49946921fe408713b85
AYun <alina.yun@ya.ru>
2022-06-25 03:14:27 +0300

    Code review

--- TeleBot.py
+++ TeleBot.py
@@ -13,7 +13,25 @@ with open('list_of_current_tasks.csv', 'r', encoding='utf-8') as file:
     reader = csv.reader(file)
     for line in reader:
         list_of_tasks.append(line)
-    print(list_of_tasks)
+
+def read_from_file(file, msg):
+    index = 1
+    with open(file, 'r', encoding='utf-8') as file:
+        reader = csv.reader(file)
+        for line in reader:
+            bot.send_message(msg.chat.id, f'{str(index)}. {line[0]}')
+            index += 1
+
+def write_to_file(file):
+    with open(file, 'w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file, delimiter=';')
+        for elem in list_of_tasks:
+            writer.writerow(elem)
+
+def append_to_file(file, msg):
+    with open (file, 'a', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file, delimiter=';')
+        writer.writerow(msg)
 
 @bot.message_handler(commands=['start'])
 def menu(message):
@@ -33,40 +51,20 @@ def msg_from_user(message):
     get_message_bot = message.text.strip().lower()
     global list_of_tasks
     if get_message_bot == 'добавить задачу':
-        sent = bot.send_message(message.chat.id, 'Напиши задачу')
+        sent = bot.send_message(message.chat.id, f'*Напиши задачу*', parse_mode='Markdown')
         bot.register_next_step_handler(sent, message_to_save)
     elif get_message_bot == 'удалить задачу':
-        sent = bot.send_message(message.chat.id, 'Какую задачу удалить (напиши ее номер)?')
-        index = 1
-        with open('list_of_current_tasks.csv', 'r', encoding='utf-8') as file:
-            reader = csv.reader(file)
-            for line in reader:
-                bot.send_message(message.chat.id, f'{str(index)}. {line[0]}')
-                index += 1
+        sent = bot.send_message(message.chat.id, f'*Какую задачу удалить (напиши ее номер)?*', parse_mode='Markdown')
+        read_from_file('list_of_current_tasks.csv', message)
         bot.register_next_step_handler(sent, message_to_delete)
     elif get_message_bot == 'задача выполнена':
-        sent = bot.send_message(message.chat.id, 'Какая задача выполнена (напиши ее номер)?')
-        index = 1
-        with open('list_of_current_tasks.csv', 'r', encoding='utf-8') as file:
-            reader = csv.reader(file)
-            for line in reader:
-                bot.send_message(message.chat.id, f'{str(index)}. {line[0]}')
-                index += 1
+        sent = bot.send_message(message.chat.id, f'*Какая задача выполнена (напиши ее номер)?*', parse_mode='Markdown')
+        read_from_file('list_of_current_tasks.csv', message)
         bot.register_next_step_handler(sent, mark_accomplished)
     elif get_message_bot == 'список текущих задач':
-        index = 1
-        with open('list_of_current_tasks.csv', 'r', encoding='utf-8') as file:
-            reader = csv.reader(file, delimiter=';')
-            for line in reader:
-                bot.send_message(message.chat.id, f'{str(index)}. {line[0]}')
-                index += 1
+        read_from_file('list_of_current_tasks.csv', message)
     elif get_message_bot == 'список завершенных задач':
-        index = 1
-        with open('list_of_accomplished_tasks.csv', 'r', encoding='utf-8') as file:
-            reader = csv.reader(file, delimiter=';')
-            for line in reader:
-                bot.send_message(message.chat.id, f'{str(index)}. {line[0]}')
-                index += 1
+        read_from_file('list_of_accomplished_tasks.csv', message)
     elif get_message_bot == 'выйти':
         markup = types.ReplyKeyboardRemove(selective=False)
         bot.send_message(message.chat.id, text='До свидания! Заходи еще!', reply_markup=markup)
@@ -77,13 +75,8 @@ def mark_accomplished(message):
     accomplished_task = message.text
     if accomplished_task.isdigit() and int(accomplished_task) <= len(list_of_tasks):
         deleted_task = list_of_tasks.pop(int(accomplished_task) - 1)
-        with open ('list_of_current_tasks.csv', 'w', newline='', encoding='utf-8') as file:
-            writer = csv.writer(file, delimiter=';')
-            for elem in list_of_tasks:
-                writer.writerow(elem)
-        with open('list_of_accomplished_tasks.csv', 'a', newline='', encoding='utf-8') as file:
-            writer = csv.writer(file, delimiter=';')
-            writer.writerow(deleted_task)
+        write_to_file('list_of_current_tasks.csv')
+        append_to_file('list_of_accomplished_tasks.csv', deleted_task)
         bot.send_message(message.chat.id, f'Задача *{deleted_task[0]}* выполнена', parse_mode='Markdown')
     elif accomplished_task.isdigit() and int(accomplished_task) > len(list_of_tasks):
         bot.send_message(message.chat.id, f'Задачи с таким номером не существует')
@@ -94,11 +87,8 @@ def message_to_delete(message):
     task_to_delete = message.text
     if task_to_delete.isdigit() and int(task_to_delete) <= len(list_of_tasks):
         deleted_task = list_of_tasks.pop(int(task_to_delete)-1)
-        bot.send_message(message.chat.id, f'Задача *{deleted_task}* удалена', parse_mode='Markdown')
-        with open('list_of_current_tasks.csv', 'w', newline='', encoding='utf-8') as file:
-            writer = csv.writer(file, delimiter=';')
-            for elem in list_of_tasks:
-                writer.writerow(elem)
+        bot.send_message(message.chat.id, f'Задача *{deleted_task[0]}* удалена', parse_mode='Markdown')
+        write_to_file('list_of_current_tasks.csv')
     elif task_to_delete.isdigit() and int(task_to_delete) > len(list_of_tasks):
         bot.send_message(message.chat.id, f'Задачи с таким номером не существует')
     else:
@@ -106,10 +96,8 @@ def message_to_delete(message):
 
 def message_to_save(message):
     message_to_save = [message.text]
-    with open ('list_of_current_tasks.csv', 'a', newline='', encoding='utf-8') as file:
-        writer = csv.writer(file)
-        writer.writerow(message_to_save)
-        bot.send_message(message.chat.id, f'Задача *{message_to_save[0]}* создана', parse_mode='Markdown')
+    append_to_file('list_of_current_tasks.csv', message_to_save)
+    bot.send_message(message.chat.id, f'Задача *{message_to_save[0]}* создана', parse_mode='Markdown')
     list_of_tasks.append(message_to_save)
 
 
